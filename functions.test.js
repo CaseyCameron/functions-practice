a0d29eedd8bed04eba846a4e73c09282288cf997
Casey Cameron <mcasey.cameron@gmail.com>
2021-06-07 16:50:12 -0700

    Backet Notation for reals Passes

--- functions.test.js
+++ functions.test.js
@@ -3,6 +3,11 @@ import { getName } from './functions';
 describe('Get a name from an object', () => {
   it('This will grab the value of a name key', () => {
     const spot = { name: "spot", age: 5, weight: "20 lbs" };
-    getName(spot);
+    const actualOne = getName(spot);
+
+    const character = { _id: "5cf5679a915ecad153ab68c9", name: "Aang" };
+    const actualTwo = getName(character);
+    expect(actualOne).toEqual('spot');
+    expect(actualTwo).toEqual('Aang');
   });
 });
